d3e8deb88207067d16426c48fa0235c54d3476b6
Iliana Vasslides <ilianamvass@gmail.com>
2025-09-24 19:16:08 -0400

    fixing loading

--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import duckdb
 import os
 import logging
+import time 
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -16,12 +17,71 @@ def load_parquet_files():
         # Connect to local DuckDB instance
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
-
+        # loading vehicle emissions csv file
         con.execute(f"""
-            -- SQL goes here
+            DROP TABLE IF EXISTS vehicle_emissions;
+            CREATE TABLE behicle_emissions AS
+            SELECT * FROM read_csv('https://raw.githubusercontent.com/ivasslides/ds3022-data-project-1/refs/heads/main/data/vehicle_emissions.csv');
+        """)
+        logging.info("Successfully loaded vehicle emissions file")
+    
+    
+        con = duckdb.connect(database='taxi_trips.duckdb', read_only=False)
+
+        # creating base url for yellow taxi trips
+        base = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet"
+
+        # create list of urls for 2015 - 2024, using base url
+        urls = [
+            base.format(year = year, month = month)
+            for year in range(2015, 2025) # 2025 is exclusive
+            for month in range(1, 13) # 13 is exclusive
+            time.sleep(60) 
+            ]
+
+        # dropping and creating main table
+        con.execute(f"""
+            DROP TABLE IF EXISTS yellow_taxi_trips;
+            CREATE TABLE yellow_taxi_trips AS
+            SELECT * FROM read_parquet('{urls[0]}');
         """)
         logger.info("Dropped table if exists")
 
+        # looping through urls and loading into table
+        for url in urls: 
+            con.execute(f"""
+                INSERT INTO yellow_taxi_trips
+                SELECT * FROM read_parquet('{url}');
+            """)
+        logging.info("Successfully loaded all tables for yellow taxis trips")
+
+        # creating base url for green taxi trips
+        green_base = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month}.parquet"
+
+        # creating list of urls for 2015 - 2024, using base url
+        green_urls = [
+            green_base.format(year = year, month = month)
+            for year in range(2015, 2025) # 2025 is exclusive
+            for month in range(1, 13) # 13 is exclusive
+            time.sleep(60)
+        ]
+
+        # creating main table if doesnt exist 
+        con.execute("""
+            DROP TABLE IF EXISTS green_taxi_trips;
+            CREATE TABLE green_taxi_trips AS
+            SELECT * FROM read_parquet('{green_urls[0]}');
+            """) 
+
+        # looping through urls and loading into table
+        for url in green_urls:
+            con.execute(f"""
+                INSERT INTO green_taxi_trips
+                SELECT * FROM read_parquet('{url}');
+            """)
+            logging.info("Successfully loaded all tables for green taxis trips")        
+    
+    
     except Exception as e:
         print(f"An error occurred: {e}")
         logger.error(f"An error occurred: {e}")
