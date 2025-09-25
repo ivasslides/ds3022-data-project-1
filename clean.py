60764e61959f05be6eaa581ff9916348c920c043
Iliana Vasslides <ilianamvass@gmail.com>
2025-09-24 22:26:28 -0400

    load and cleaning edits

--- clean.py
+++ clean.py
@@ -1,2 +1,140 @@
 import duckdb
 import logging
+
+def cleaning():
+
+    con = None
+
+    try:
+        # Connect to local DuckDB
+        con = duckdb.connect(database='taxi_trips.duckdb', read_only=False)
+
+        # remove any duplicate trips
+        con.execute(f"""
+            CREATE TABLE yellow_clean AS
+            SELECT DISTINCT VectorId FROM yellow_taxi_trips; 
+            DROP TABLE yellow_taxi_trips;
+            ALTER TABLE yellow_clean RENAME TO yellow_taxi_trips; 
+
+            CREATE TABLE green_clean AS
+            SELECT DISTINCT VectorId FROM green_taxi_trips; 
+            DROP TABLE green_taxi_trips;
+            ALTER TABLE green_clean RENAME TO green_taxi_trips;
+        """)
+        # testing
+        dup_yellow = con.execute("""
+            SELECT COUNT(*) FROM
+                (SELECT VectorID, COUNT(*) AS occurrences
+                FROM yellow_taxi_trips
+                GROUP BY VectorId
+                HAVING COUNT(*) > 1)
+        """)
+        dup_green = con.execute("""
+            SELECT COUNT(*) FROM
+                (SELECT VectorID, COUNT(*) AS occurrences
+                FROM green_taxi_trips
+                GROUP BY VectorId
+                HAVING COUNT(*) > 1)
+        """)
+
+        if dup_yellow.fetchone()[0] == 0 and dup_green.fetchone()[0] == 0:
+            logging.info("No duplicate trips.")
+        else:
+            logging.info("There is a duplicate trip remaining.")
+
+        # remove any trips with 0 passengers
+        con.execute(f"""
+            DELETE FROM yellow_taxi_trips
+            WHERE passenger_count = 0;
+
+            DELETE FROM green_taxi_trips
+            WHERE passenger_count = 0; 
+        """)
+        # testing
+        pass_yellow = con.execute(f"""
+            SELECT MIN(passenger_count) FROM yellow_taxi_trips;""")
+        pass_green = con.execute(f"""
+            SELECT MIN(passenger_count) FROM green_taxi_trips;""")
+            
+        if pass_yellow.fetchone()[0] != 0 and pass_green.fetchone()[0] != 0:
+            logging.info("All trips have at least 1 passenger.")
+        else:
+            logging.info("There is a trip with no passengers remaining.")
+
+        # remove any trips 0 miles in length
+        con.execute(f"""
+            DELETE FROM yellow_taxi_trips
+            WHERE trip_distance = 0;
+
+            DELETE FROM green_taxi_trips
+            WHERE trip_distance = 0;            
+        """)
+        # testing
+        min_length_yellow = con.execute(f"""
+            SELECT MIN(trip_distance) FROM yellow_taxi_trips;""")
+        min_length_green = con.execute(f"""
+            SELECT MIN(trip_distance) FROM green_taxi_trips;""")
+            
+        if min_length_yellow.fetchone()[0] != 0 and min_length_green.fetchone()[0] != 0:
+            logging.info("All trips are longer than 0 miles.")
+        else:
+            logging.info("There is a trip that is 0 miles long remaining.")
+
+        # remove any trips longer than 100 miles in length
+        con.execute(f"""
+            DELETE FROM yellow_taxi_trips
+            WHERE trip_distance > 100;
+
+            DELETE FROM green_taxi_trips
+            WHERE trip_distance > 100;            
+        """)
+        # testing 
+        max_length_yellow = con.execute(f"""
+            SELECT MAX(trip_distance) FROM yellow_taxi_trips;""")
+        max_length_green = con.execute(f"""
+            SELECT MAX(trip_distance) FROM green_taxi_trips;""")
+            
+        if max_length_yellow.fetchone()[0] <= 100 and max_length_green.fetchone()[0] <= 100:
+            logging.info("All trips are shorter than 100 miles.")
+        else:
+            logging.info("There is a trip longer than 100 miles remaining.")
+
+        # remove any trips lasting more than 1 day in length (86400 seconds)
+        con.execute(f"""
+        -- calculate trip time yellow
+        SELECT tpep_pickup_datetime, tpep_dropoff_datetime, 
+            DATE_DIFF('second', tpep_pickup_datetime, tpep_dropoff_datetime)
+            AS trip_seconds
+        FROM yellow_taxi_trips;
+        -- remove when >86400
+        DELETE FROM yellow_taxi_trips
+        WHERE trip_seconds > 86400;
+
+        -- calculate trip time green
+        SELECT tpep_pickup_datetime, tpep_dropoff_datetime, 
+            DATE_DIFF('second', tpep_pickup_datetime, tpep_dropoff_datetime)
+            AS trip_seconds
+        FROM green_taxi_trips;
+        -- remove when >86400
+        DELETE FROM green_taxi_trips
+        WHERE trip_seconds > 86400;            
+        """)
+        # testing 
+        max_time_yellow = con.execute(f"""
+            SELECT MAX(trip_seconds) FROM yellow_taxi_trips;""")
+        max_time_green = con.execute(f"""
+            SELECT MAX(trip_seconds) FROM green_taxi_trips;""")
+
+        if max_time_yellow.fetchone()[0] <= 86400 and max_time_green.fetchone()[0] <= 86400:
+            logging.info("All trips are shorter than 1 day.")
+        else:
+            logging.info("There is a trip longer than 1 day remaining.")
+
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    cleaning()
+
--- load.py
+++ load.py
@@ -20,66 +20,73 @@ def load_parquet_files():
         # loading vehicle emissions csv file
         con.execute(f"""
             DROP TABLE IF EXISTS vehicle_emissions;
-            CREATE TABLE behicle_emissions AS
+            CREATE TABLE vehicle_emissions AS
             SELECT * FROM read_csv('https://raw.githubusercontent.com/ivasslides/ds3022-data-project-1/refs/heads/main/data/vehicle_emissions.csv');
         """)
         logging.info("Successfully loaded vehicle emissions file")
     
-    
+
         con = duckdb.connect(database='taxi_trips.duckdb', read_only=False)
 
         # creating base url for yellow taxi trips
-        base = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet"
+        base = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet"
 
         # create list of urls for 2015 - 2024, using base url
         urls = [
             base.format(year = year, month = month)
             for year in range(2015, 2025) # 2025 is exclusive
-            for month in range(1, 13) # 13 is exclusive
-            time.sleep(60) 
+            for month in range(1, 13) # 13 is exclusive 
             ]
 
         # dropping and creating main table
         con.execute(f"""
             DROP TABLE IF EXISTS yellow_taxi_trips;
             CREATE TABLE yellow_taxi_trips AS
-            SELECT * FROM read_parquet('{urls[0]}');
+            SELECT VendorId, passenger_count, trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime
+            FROM read_parquet('{urls[0]}');
         """)
-        logger.info("Dropped table if exists")
+        logger.info("Dropped yellow_taxi_trips if exists")
 
         # looping through urls and loading into table
         for url in urls: 
             con.execute(f"""
                 INSERT INTO yellow_taxi_trips
-                SELECT * FROM read_parquet('{url}');
+                SELECT VendorId, passenger_count, trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime
+                FROM read_parquet('{url}');
             """)
+            time.sleep(60)
         logging.info("Successfully loaded all tables for yellow taxis trips")
 
         # creating base url for green taxi trips
-        green_base = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month}.parquet"
+        green_base = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet"
 
         # creating list of urls for 2015 - 2024, using base url
         green_urls = [
             green_base.format(year = year, month = month)
             for year in range(2015, 2025) # 2025 is exclusive
             for month in range(1, 13) # 13 is exclusive
-            time.sleep(60)
         ]
 
         # creating main table if doesnt exist 
         con.execute("""
             DROP TABLE IF EXISTS green_taxi_trips;
             CREATE TABLE green_taxi_trips AS
-            SELECT * FROM read_parquet('{green_urls[0]}');
+            SELECT VendorId, passenger_count, trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime
+            FROM read_parquet('{green_urls[0]}');
             """) 
+        
+        logger.info("Dropped green_taxi_trips if exists")
 
         # looping through urls and loading into table
         for url in green_urls:
             con.execute(f"""
                 INSERT INTO green_taxi_trips
-                SELECT * FROM read_parquet('{url}');
+                SELECT VendorId, passenger_count, trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime
+                FROM read_parquet('{url}');
             """)
-            logging.info("Successfully loaded all tables for green taxis trips")        
+            time.sleep(60)
+
+        logging.info("Successfully loaded all tables for green taxis trips")        
     
     
     except Exception as e:
